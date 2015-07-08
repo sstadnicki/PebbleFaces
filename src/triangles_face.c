eb27a04ef75715d470e4ca669908752bca2ad3b3
sstadnicki <sstadnicki@gmail.com>
2015-07-08 08:51:44 -0700

    Added many more Halton 'shuffles' (esp. color)

--- src/triangles_face.c
+++ src/triangles_face.c
@@ -1,12 +1,11 @@
 #include <pebble.h>
   
-#define NUM_BACKGROUND_TRIANGLE_SETS 3
-#define GRID_SIZE 5
-#define NUM_BACKGROUND_TRIANGLES (NUM_BACKGROUND_TRIANGLE_SETS*GRID_SIZE)
+#define NUM_BACKGROUND_TRIANGLES 18
 #define HOUR_HAND_LENGTH 30
 #define MINUTE_HAND_LENGTH (2*HOUR_HAND_LENGTH)
 #define EDGE_WIDTH 4
-static bool FILL_TRIS=false;
+static bool FILL_TRIS=true;
+static bool RANDOMIZE_SIZE=true;
 #define MIN_HOUR_HAND_LENGTH (HOUR_HAND_LENGTH/2)
 #define MAX_HOUR_HAND_LENGTH (2*HOUR_HAND_LENGTH)
 
@@ -39,40 +38,67 @@ static Layer * s_canvas_layer;
 // The center of the display
 GPoint s_center;
 
-// Generate a random bright color - this is done by randomly choosing a point
-// on the 'middle edges' of the RGB cube - that is, those between the solid
-// 'primary' colors (R, G, B) and the solid 'secondary' colors (C, M, Y).
-static void generateRandomBrightColor(GColor *outColor) {
+#define NUM_PRIMES 4
+static int s_primes[NUM_PRIMES] = {2, 3, 5, 7};
+
+// Computes a 'Halton value' given a number and a base - this is the 'reverse'
+// of the number, in the given base, as a value between 0 and 1.
+static float generateHaltonValue(int number, int base) {
+  int temp_number = number;
+  float inverse_base = 1.0f/((float)base);
+  float current_position = inverse_base;
+  float output = 0.0f;
+  while ( temp_number > 0 ) {
+    output += current_position * (temp_number % base);
+    temp_number /= base;
+    current_position *= inverse_base;
+  }
+  return output;
+}
+
+// Generate a bright color.  This is done by converting the float 'hue angle'
+// (in the range [0..1] to a point on the 'middle edges' of the RGB cube - 
+// that is, those between the solid 'primary' colors (R, G, B) and the solid
+// 'secondary' colors (C, M, Y).  (This is approximately equal to choosing a
+// point with the passed-in value as hue, full saturation and full value.)
+static void generateBrightColor(float hue_angle, GColor *out_color) {
   unsigned r=0, g=0, b=0;
-  unsigned edge = rand() % 6;
-  unsigned dist = rand() % 256;
+  float expanded_hue = 6.0f * hue_angle;
+  int edge = (unsigned) (expanded_hue);
+  if ( (edge < 0) || (edge > 5) ) {
+    // our input value was out of range; return black as a 'sentinel'
+    *out_color = GColorBlack;
+    return;
+  }
+  int dist = (unsigned) (256.0f*(expanded_hue-edge));
   switch (edge) {
   case 0: // (1,0,0) -> (1, 1, 0)
     r = 255; g = dist; b = 0;
     break;
   case 1: // (1, 1, 0) -> (0, 1, 0)
-    r = dist; g = 255; b = 0;
+    r = 255-dist; g = 255; b = 0;
     break;
   case 2: // (0, 1, 0) -> (0, 1, 1)
     r = 0; g = 255; b = dist;
     break;
   case 3: // (0, 1, 1) -> (0, 0, 1)
-    r = 0; g = dist; b = 255;
+    r = 0; g = 255-dist; b = 255;
     break;
   case 4: // (0, 0, 1) -> (1, 0, 1)
     r = dist; g = 0; b = 255;
     break;
   case 5: // (1, 0, 1) -> (1, 0, 0)
-    r = 255; g = 0; b = dist;
+    r = 255; g = 0; b = 255-dist;
     break;
   }
-  *outColor = GColorFromRGB(r,g,b);
+  *out_color = GColorFromRGB(r,g,b);
 }
 
 // Create a clock triangle from the appropriate parameters
 static void generateClockTriangle(
   int hour,
   int minute,
+  float size,
   GColor color,
   GPoint center,
   ClockTriangle *out_triangle)
@@ -83,12 +109,14 @@ static void generateClockTriangle(
   float hour_angle = TRIG_MAX_ANGLE * hour / 12;
   // Make sure to advance the hour angle by the number of minutes!
   hour_angle += minute_angle/12;
+  int hour_hand_length = (int) (size*HOUR_HAND_LENGTH);
+  int minute_hand_length = (int) (size*MINUTE_HAND_LENGTH);
   // Position the hour hand
-  out_triangle->hour_hand.x = center.x + (sin_lookup(hour_angle) * HOUR_HAND_LENGTH)/TRIG_MAX_RATIO;
-  out_triangle->hour_hand.y = center.y - (cos_lookup(hour_angle) * HOUR_HAND_LENGTH)/TRIG_MAX_RATIO;
+  out_triangle->hour_hand.x = center.x + (sin_lookup(hour_angle) * hour_hand_length)/TRIG_MAX_RATIO;
+  out_triangle->hour_hand.y = center.y - (cos_lookup(hour_angle) * hour_hand_length)/TRIG_MAX_RATIO;
   // Position the minute hand
-  out_triangle->minute_hand.x = center.x + (sin_lookup(minute_angle) * MINUTE_HAND_LENGTH)/TRIG_MAX_RATIO;
-  out_triangle->minute_hand.y = center.y - (cos_lookup(minute_angle) * MINUTE_HAND_LENGTH)/TRIG_MAX_RATIO;
+  out_triangle->minute_hand.x = center.x + (sin_lookup(minute_angle) * minute_hand_length)/TRIG_MAX_RATIO;
+  out_triangle->minute_hand.y = center.y - (cos_lookup(minute_angle) * minute_hand_length)/TRIG_MAX_RATIO;
 }
   
 // Render a triangle to the graphics context
@@ -118,10 +146,12 @@ static void drawClockFace(GContext *ctx) {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   GColor random_color;
-  generateRandomBrightColor(&random_color);
+  float random_hue = ((float) rand())/((float) RAND_MAX);
+  generateBrightColor(random_hue, &random_color);
   generateClockTriangle(
     tick_time->tm_hour,
     tick_time->tm_min,
+    1.0f,
     random_color,
     s_center,
     &s_foreground_triangle);
@@ -133,41 +163,42 @@ static void drawClockFace(GContext *ctx) {
 }
 
 static void initBackgroundTriangles() {
-  static int shuffle_array[GRID_SIZE];
-  int tri_idx=0;
-  for (int idx = 0; idx < GRID_SIZE; idx++)
-    shuffle_array[idx] = idx;
-  for (int set_idx = 0; set_idx < NUM_BACKGROUND_TRIANGLE_SETS; set_idx++) {
-    // shuffle our array
-    for (int shuffle_idx = 0; shuffle_idx < GRID_SIZE-1; shuffle_idx++) {
-      int other_idx = shuffle_idx+(rand() % (GRID_SIZE-1-shuffle_idx));
-      int temp = shuffle_array[shuffle_idx];
-      shuffle_array[shuffle_idx] = shuffle_array[other_idx];
-      shuffle_array[other_idx] = temp;
-    }
-    // Now add triangles in the various grid boxes
-    for (int grid_idx = 0; grid_idx < GRID_SIZE; grid_idx++) {
-      int x_offset = (144*grid_idx)/GRID_SIZE;
-      int y_offset = (168*shuffle_array[grid_idx])/GRID_SIZE;
-      x_offset += (rand()%(144/GRID_SIZE));
-      y_offset += (rand()%(168/GRID_SIZE));
-      GColor random_color;
-      generateRandomBrightColor(&random_color);
-      int random_hour = rand() % 12;
-      int random_minutes = random_hour*5;
-      int minuteoffset = 4+(rand()%19); // 4..22 - a restriction of 0..30
-      random_minutes = (rand()&1) ? random_minutes-minuteoffset : random_minutes+minuteoffset;
-      random_minutes = (random_minutes+60)%60; // clamp to 0..60 range
-      generateClockTriangle(
-        random_hour,
-        random_minutes,
-        random_color,
-        (GPoint) {.x = x_offset, .y = y_offset},
-        &(s_background_triangles[tri_idx])
-      );
-      tri_idx++;
-  
-    }
+  // Decide on a 'starting value' for our Halton grid
+  int halton_index = 256+(rand()&127);
+  // shuffle our primes
+  for (int prime_idx = 0; prime_idx < NUM_PRIMES-1; prime_idx++) {
+    int random_idx = rand() % (4-prime_idx);
+    int temp = s_primes[prime_idx];
+    s_primes[prime_idx] = s_primes[random_idx];
+    s_primes[random_idx] = temp;
+  }
+  int x_base = s_primes[0];
+  int y_base = s_primes[1];
+  int size_base = s_primes[2];
+  int color_base = s_primes[3];
+  // Now build all the triangles
+  for (int tri_idx = 0; tri_idx < NUM_BACKGROUND_TRIANGLES; tri_idx++) {
+    int x_offset = (int) (144.0f * generateHaltonValue(halton_index, x_base));
+    int y_offset = (int) (168.0f * generateHaltonValue(halton_index, y_base));
+    float size_uniform = generateHaltonValue(halton_index, size_base);
+    float random_size = (size_uniform < 0.5f) ? size_uniform+0.5f : size_uniform*2.0f;
+    float random_hue = generateHaltonValue(halton_index, color_base);
+    GColor random_color;
+    generateBrightColor(random_hue, &random_color);
+    int random_hour = rand() % 12;
+    int random_minutes = random_hour*5;
+    int minuteoffset = 4+(rand()%19); // 4..22 - a restriction of 0..30
+    random_minutes = (rand()&1) ? random_minutes-minuteoffset : random_minutes+minuteoffset;
+    random_minutes = (random_minutes+60)%60; // clamp to 0..60 range
+    generateClockTriangle(
+      random_hour,
+      random_minutes,
+      (RANDOMIZE_SIZE)?random_size:1.0f,
+      random_color,
+      (GPoint) {.x = x_offset, .y = y_offset},
+      &(s_background_triangles[tri_idx])
+    );
+    halton_index++;
   }
 }
 
